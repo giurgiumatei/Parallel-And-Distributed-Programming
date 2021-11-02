ce73419b08560e890408a7cf29d0edd5f594d662
Giurgiu Matei <giurgiu.matei@yahoo.com>
2021-11-02 17:42:07 +0200

    Add files via upload

---
+++ Lab3/Lab3/Lab3/Domain/MatrixCalculator.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab3.Domain
+{
+    public class MatrixCalculator
+    {
+        private static readonly object Lock = new();
+        public static void ComputeTask(List<List<int>> matrix1, List<List<int>> matrix2, List<int> coordinate, int numberOfColumns ,List<List<int>> result)
+        {
+            lock (Lock)
+            {
+                int product = 0;
+                int row = coordinate[0];
+                int column = coordinate[1];
+
+                for (int i = 0; i < numberOfColumns; i++)
+                {
+                    Interlocked.Add(ref product, matrix1[row][i] * matrix2[i][column]);
+                }
+
+                result[row][column] = product;
+            }
+        }
+
+        public static  void Compute(List<List<int>> matrix1, List<List<int>> matrix2, List<List<int>> batch, int numberOfColumns, List<List<int>> result)
+        {
+            batch.ForEach(pair =>
+            {
+                lock (Lock)
+                {
+                    int product = 0;
+
+                    int row = pair[0];
+                    int column = pair[1];
+
+                    for (int i = 0; i < numberOfColumns; i++)
+                    {
+                        Interlocked.Add(ref product, matrix1[row][i] * matrix2[i][column]);
+                    }
+
+                    result[row][column] = product;
+                }
+            });
+        }
+
+        public static List<List<int>> InitializeMatrix(int numberOfRows, int numberOfColumns)
+        {
+            var resultingMatrix = new List<List<int>>();
+
+            for (int i = 0; i < numberOfRows; i++)
+            {
+                var row = Enumerable.Range(1, numberOfColumns).ToList();
+                resultingMatrix.Add(row);
+            }
+
+            return resultingMatrix;
+        }
+
+        public static void PrintMatrix(List<List<int>> matrix)
+        {
+            matrix.ForEach(row =>
+            {
+                row.ForEach(element => Console.Write(element + " "));
+                Console.Write("\n");
+            });
+        }
+
+        public static List<List<int>> GetCoordinatesOfMatrix(int matrixRows, int matrixColumns)
+        {
+            List<List<int>> coordinates = new();
+            
+            for (int i = 0; i < matrixRows; i++)
+            {
+                for (int j = 0; j < matrixColumns; j++)
+                {
+                    coordinates.Add(new List<int>{i, j});
+                }
+            }
+            return coordinates;
+        }
+
+    }
+}
---
+++ Lab3/Lab3/Lab3/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Lab3.Domain;
+
+namespace Lab3
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int numberOfRows = 5;
+            int numberOfColumns = 5;
+            
+            var matrix1 = MatrixCalculator.InitializeMatrix(numberOfRows, numberOfColumns);
+            var matrix2 = MatrixCalculator.InitializeMatrix(numberOfRows, numberOfColumns);
+            var result = MatrixCalculator.InitializeMatrix(numberOfRows, numberOfColumns);
+
+            var coordinates = MatrixCalculator.GetCoordinatesOfMatrix(numberOfRows, numberOfColumns);
+
+            List<Thread> threads = new();
+            int threadCount = 4;
+            int batchSize = coordinates.Count / threadCount;
+
+            List<List<List<int>>> batches = new();
+
+            for (int i = 0; i < coordinates.Count - batchSize; i+= batchSize)
+            {
+                batches.Add(coordinates.GetRange(i, batchSize));
+            }
+
+            int remaining = coordinates.Count % threadCount;
+            for (int i = 0; i < remaining; i++)
+            {
+                var remain = coordinates[coordinates.Count - i - 1];
+                batches[i].Add(remain);
+            }
+
+            //   1.Create an actual thread for each task (use the low - level thread mechanism from the programming language);
+            // batches.ForEach(batch => {
+            //     threads.Add(new Thread(()=> {
+            //         MatrixCalculator.Compute(matrix1, matrix2, batch, numberOfColumns, result);
+            //     }));
+            // });
+            //
+            // threads.ForEach(thread => thread.Start());
+            // threads.ForEach(thread => thread.Join());
+            //
+            // result.ForEach(row =>
+            // {
+            //     row.ForEach(element => Console.Write(element + " "));
+            //     Console.WriteLine();
+            // });
+
+
+
+            // 2.Use a thread pool
+            coordinates.ForEach(coordinate =>
+            {
+                ThreadPool.QueueUserWorkItem(_ =>
+                    MatrixCalculator.ComputeTask(matrix1, matrix2, coordinate, numberOfColumns, result));
+            });
+
+            // Initialize counter with 1, otherwise, the AddCount method will throw an error (the counter is signaled and cannot be incremented)-
+            using (CountdownEvent countdownEvent = new CountdownEvent(1))
+            {
+                ManualResetEvent manualResetEvent = new ManualResetEvent(false);
+
+                coordinates.ForEach(coordinate =>
+                {
+                    ThreadPool.QueueUserWorkItem(state =>
+                    {
+                        // Increase the counter
+                        countdownEvent.AddCount();
+                        // Execute the business logic: a thread-safe withdraw books (implements lock internally)
+                        MatrixCalculator.ComputeTask(matrix1, matrix2, coordinate, numberOfColumns, result);
+                        // Decrease the counter
+                        countdownEvent.Signal();
+                        // Signal that at least one thread is executed.
+                        manualResetEvent.Set();
+                    });
+                });
+
+                // ensure that at least one thread was created 
+                manualResetEvent.WaitOne();
+
+                // Decrease the counter (as it was initialized with the value 1).
+                countdownEvent.Signal();
+
+                // Wait until the counter is zero.
+                countdownEvent.Wait();
+
+            }
+
+            result.ForEach(row =>
+             {
+                 row.ForEach(element => Console.Write(element + " "));
+                 Console.WriteLine();
+             });
+        }
+    }
+}
