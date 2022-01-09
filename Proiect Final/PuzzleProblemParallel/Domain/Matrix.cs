943787760ae155c145fe211bb03a9f123262806e
Giurgiu Matei <giurgiu.matei@yahoo.com>
2022-01-09 17:15:20 +0200

    added final project

---
+++ Proiect Final/PuzzleProblemParallel/Domain/Matrix.cs
@@ -0,0 +1,145 @@
+﻿using System.Text;
+
+namespace PuzzleProblemParallel.Domain;
+
+[Serializable]
+public sealed class Matrix
+{
+    private Matrix? _previousState;
+    private static int[] _dx = { 0, -1, 0, 1 };
+    private static int[] _dy = { -1, 0, 1, 0 };
+    private static string[] _movesStrings = { "left", "up", "right", "down" };
+    
+    public byte[][] Tiles { get; set; }
+    public int FreePositionI { get; set; }
+    public int FreePositionJ { get; set; }
+    public int NumberOfSteps { get; set; }
+    public int MinimumNumberOfSteps { get; set; }
+    public int Estimation { get; set; }
+    public int Manhattan { get; set; }
+    public string Move { get; set; }
+
+
+
+    public Matrix(byte[][] tiles, int freePositionI, int freePositionJ, int numberOfSteps, Matrix? previousState, string move)
+    {
+        Tiles = tiles;
+        FreePositionI = freePositionI;
+        FreePositionJ = freePositionJ;
+        NumberOfSteps = numberOfSteps;
+        _previousState = previousState;
+        Move = move;
+        Manhattan = ManhattanDistance();
+        MinimumNumberOfSteps = numberOfSteps + Manhattan;
+        Estimation = Manhattan + numberOfSteps;
+    }
+
+    public static Matrix FromFile()
+    {
+        var tiles = new byte[4][];
+
+        for (var i = 0; i < 4; i++)
+        {
+            tiles[i] = new byte[4];
+        }
+
+        tiles = File.ReadAllLines("C:\\Facultate\\Anul 3\\PDP\\Proiect\\Parallel-And-Distributed-Programming\\Proiect\\PuzzleProblemParallel\\PuzzleProblemParallel\\Domain\\Matrix.txt")
+            .Select(l => l.Split(' ').Select(byte.Parse).ToArray())
+            .ToArray();
+
+        var freeI = -1;
+        var freeJ = -1;
+
+        for (var i = 0; i < 4; i++)
+        {
+            for (var j = 0; j < 4; j++)
+            {
+                if (tiles[i][j] != 0) continue;
+
+                freeI = i;
+                freeJ = j;
+            }
+        }
+
+        return new Matrix(tiles, freeI, freeJ, 0, null, string.Empty);
+    }
+
+
+    public int ManhattanDistance()
+    {
+        var distance = 0;
+        for (var i = 0; i < 4; i++)
+        {
+            for (var j = 0; j < 4; j++)
+            {
+                if (Tiles[i][j] == 0) continue;
+                
+                var targetI = (Tiles[i][j] - 1) / 4;
+                var targetJ = (Tiles[i][j] - 1) % 4;
+                distance += Math.Abs(i - targetI) + Math.Abs(j - targetJ);
+            }
+        }
+
+        return distance;
+    }
+
+    public List<Matrix> GenerateMoves()
+    {
+        List<Matrix> moves = new();
+
+        for (var k = 0; k < 4; k++)
+        {
+            if (FreePositionI + _dx[k] < 0 || FreePositionI + _dx[k] >= 4 || FreePositionJ + _dy[k] < 0 ||
+                FreePositionJ + _dy[k] >= 4) continue;
+
+            var movedFreePositionI = FreePositionI + _dx[k];
+            var movedFreePositionJ = FreePositionJ + _dy[k];
+
+            if (_previousState != null && movedFreePositionI == _previousState.FreePositionI &&
+                movedFreePositionJ == _previousState.FreePositionJ) continue;
+
+            var movedTiles = Tiles.Select(x => x.ToArray()).ToArray();
+
+            movedTiles[FreePositionI][FreePositionJ] = movedTiles[movedFreePositionI][movedFreePositionJ];
+            movedTiles[movedFreePositionI][movedFreePositionJ] = 0;
+
+            moves.Add(new Matrix(movedTiles, movedFreePositionI, movedFreePositionJ, NumberOfSteps + 1, this, _movesStrings[k]));
+        }
+
+        return moves;
+    }
+
+
+    public override string ToString()
+    {
+        var current = this;
+        var result = new List<string>();
+        while (current != null)
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append(Environment.NewLine);
+            stringBuilder.Append(current.Move);
+            stringBuilder.Append(Environment.NewLine);
+        
+            for (var i = 0; i < 4; i++)
+            {
+                for (var j = 0; j < 4; j++)
+                {
+                    stringBuilder.Append(current.Tiles[i][j]);
+                    stringBuilder.Append(" ");
+                }
+                stringBuilder.Append(Environment.NewLine);
+            }
+        
+            stringBuilder.Append(Environment.NewLine);
+            result.Add(stringBuilder.ToString());
+            current = current._previousState;
+        }
+        
+        result.Reverse();
+        
+        return "Moves: " +
+               string.Join("", result) +
+               "numberOfSteps=" + NumberOfSteps;
+    }
+}
---
+++ Proiect Final/PuzzleProblemParallel/Program.cs
@@ -0,0 +1,129 @@
+﻿using PuzzleProblemParallel.Domain;
+using System.Diagnostics;
+
+
+
+var initialState = Matrix.FromFile();
+var stopWatch = Stopwatch.StartNew();
+var solution = Solve(initialState);
+
+stopWatch.Stop();
+Console.WriteLine(solution);
+Console.WriteLine($"Run time: {stopWatch.Elapsed.TotalMilliseconds} ms");
+
+static Matrix Solve(Matrix root)
+{
+    var stopWatch = Stopwatch.StartNew();
+    var minimumBound = root.Manhattan;
+
+    while (true)
+    {
+        var (minimumDistance, state) = SearchParallel(root, 0, minimumBound, 5);
+
+        if (minimumDistance == -1)
+        {
+            stopWatch.Stop();
+            Console.WriteLine($"Solution found in {state.NumberOfSteps} steps");
+            Console.WriteLine($"Execution time was {stopWatch.Elapsed.TotalMilliseconds} ms");
+            return state;
+        }
+
+        Console.WriteLine($"Depth + {minimumDistance} reached in {stopWatch.Elapsed.TotalMilliseconds} ms");
+
+        minimumBound = minimumDistance;
+    }
+}
+
+static KeyValuePair<int, Matrix> SearchParallel(Matrix current, int numberOfSteps, int bound, int numberOfTasks)
+{
+    if (numberOfTasks <= 1)
+    {
+        return Search(current, numberOfSteps, bound);
+    }
+
+    var estimation = numberOfSteps + current.Manhattan;
+
+    if (estimation > bound)
+    {
+        return new KeyValuePair<int, Matrix>(estimation, current);
+    }
+
+    if (estimation > 80)
+    {
+        return new KeyValuePair<int, Matrix>(estimation, current);
+    }
+
+    if (current.Manhattan == 0)
+    {
+        return new KeyValuePair<int, Matrix>(-1, current);
+    }
+
+    var minimum = int.MaxValue;
+    var moves = current.GenerateMoves();
+
+    var tasks = moves
+        .Select(next => new Task<KeyValuePair<int, Matrix>>(() => SearchParallel(next, numberOfSteps + 1, bound, numberOfTasks / moves.Count)))
+        .ToList();
+
+    foreach (var task in tasks)
+    {
+        task.Start();
+    }
+
+    foreach (var task in tasks)
+    {
+        var (candidate, state) = task.Result;
+
+        if (candidate == -1)
+        {
+            return new KeyValuePair<int, Matrix>(-1, state);
+        }
+
+        if (candidate < minimum)
+        {
+            minimum = candidate;
+        }
+    }
+
+    return new KeyValuePair<int, Matrix>(minimum, current);
+}
+
+static KeyValuePair<int, Matrix> Search(Matrix current, int numberOfSteps, int bound)
+{
+    var estimation = numberOfSteps + current.Manhattan;
+
+    if (estimation > bound)
+    {
+        return new KeyValuePair<int, Matrix>(estimation, current);
+    }
+
+    if (estimation > 80)
+    {
+        return new KeyValuePair<int, Matrix>(estimation, current);
+    }
+
+    if (current.Manhattan == 0)
+    {
+        return new KeyValuePair<int, Matrix>(-1, current);
+    }
+
+    var minimum = int.MaxValue;
+    Matrix solution = null;
+
+    foreach (var next in current.GenerateMoves())
+    {
+        var (candidate, state) = Search(next, numberOfSteps + 1, bound);
+
+        if (candidate == -1)
+        {
+            return new KeyValuePair<int, Matrix>(-1, state);
+        }
+
+        if (candidate >= minimum) continue;
+
+        minimum = candidate;
+        solution = state;
+    }
+
+    return new KeyValuePair<int, Matrix>(minimum, solution);
+}
