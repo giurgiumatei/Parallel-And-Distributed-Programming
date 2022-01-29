ffe62eeda038d41bf58c48c412fc0d42d5aec83d
DESKTOP-3L3V23H\giurg <giurgiu.matei@yahoo.com>
2022-01-29 18:59:00 +0200

    added permutation problem

---
+++ Pregatire Examen/Aranjamente/Aranjamente/Aranjamente/Program.cs
@@ -0,0 +1,49 @@
+﻿var n = 5;
+var k = 3;
+var array = new int[10];
+Back(array, n, k, 1);
+
+
+
+void Print(int[] array, int k)
+{
+    for (int i = 0; i < k; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+static bool Distinct(int[] elements, int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        if (elements[i] == elements[n])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void Back(int[] array, int n, int k, int i)
+{
+    for (int j = 1; j <= n; j++)
+    {
+        array[i] = j;
+
+        if (i == k)
+        {
+            if (Distinct(array, i))
+            {
+                Print(array, k);
+            }
+        }
+        else
+        {
+            Back(array, n, k, i + 1);
+        }
+    }
+}
---
+++ Pregatire Examen/PermutationsOfN/NumberOfPermutations/NumberOfPermutations/Program.cs
@@ -0,0 +1,49 @@
+﻿
+var numbers = Enumerable.Range(1, 5).ToArray();
+var tasks = new List<Task>();
+
+PrintResult(Permute(numbers, tasks));
+
+static IList<IList<int>> Permute(int[] numbers, List<Task> tasks)
+{
+    var list = new List<IList<int>>();
+    return DoPermute(numbers, 0, numbers.Length - 1, list, tasks);
+}
+
+static IList<IList<int>> DoPermute(int[] numbers, int start, int end, IList<IList<int>> list, List<Task> tasks)
+{
+    if (start == end)
+    {
+        // We have one of our possible n! solutions,
+        // add it to the list.
+        list.Add(new List<int>(numbers));
+    }
+    else
+    {
+        for (var i = start; i <= end; i++)
+        {
+            Swap(ref numbers[start], ref numbers[i]);
+            DoPermute(numbers, start + 1, end, list, tasks);
+            Swap(ref numbers[start], ref numbers[i]);
+        }
+    }
+
+    return list;
+}
+
+static void Swap(ref int a, ref int b)
+{
+    (a, b) = (b, a);
+}
+
+static void PrintResult(IList<IList<int>> lists)
+{
+    Console.WriteLine("[");
+    foreach (var list in lists)
+    {
+        Console.WriteLine($"    [{string.Join(',', list)}]");
+    }
+    Console.WriteLine("]");
+}
+
+static bool Pred(List<int> numbers) => numbers.SequenceEqual(Enumerable.Range(1, 5).ToList());
